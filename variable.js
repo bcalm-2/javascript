ec7b75852443b9fc972ae661deedff3794130982
DwivediSrashti6@gmail.com <DwivediSrashti6@gmail.com>
2024-09-22 12:48:23 +0530

    Add variable.js

---
+++ variable.js
@@ -0,0 +1,22 @@
+//To define constant:
+const PI = 3.14;
+
+//To define variable (change)
+let g = 10;
+console.log(g)
+g = 9.8;
+console.log(g)
+
+//Old way of defining variables
+var a = 10;
+a = 9.8;
+
+// Syntax:
+// To declare a variable
+// const/let/var variable_name;
+
+//To Assign value
+// variable_name = value;
+
+//To declare and assign
+// const/let/var variable_name = value;
