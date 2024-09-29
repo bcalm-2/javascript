d1b9a2ad922ccf7850fb9ee5792c681035e164c2
DwivediSrashti6@gmail.com <DwivediSrashti6@gmail.com>
2024-09-29 20:04:47 +0530

    Add arrow function

---
+++ arrowFunction.js
@@ -0,0 +1,28 @@
+// [1, -3, 4].filter((number) => {
+//   return number > 0 && number < 2;
+// });
+// [1, -3, 4].map((number) => {
+//   return number * number;
+// });
+// [1, -3, 4].filter((number) => {
+//   return number > 0;
+// });
+// [1, -3, 4].filter((number) => {
+//   return number % 2 === 0;
+// });
+// const students = [
+//   {
+//     name: "vikram",
+//     id: 1,
+//   },
+// ];
+// const studentId = Number.parseInt(prompt("Enter student id"));
+// const result = students
+//   .filter((number) => {
+//     return number.id === studentId;
+//   })
+//   .map((student) => student.name);
+// console.log(result);
+
+[1, -3, 4].filter((number) => number > 0 && number < 2);
+
